b7c0d83890b0c6585ceccecd932d1079ae54a6c2
erins98 <67079589+erins98@users.noreply.github.com>
2024-03-09 20:01:35 -0500

    First commit

---
+++ src/templates/Logo.tsx
@@ -0,0 +1,21 @@
+import { AppConfig } from '../utils/AppConfig';
+
+type ILogoProps = {
+  xl?: boolean;
+};
+
+const Logo = (props: ILogoProps) => {
+  const size = props.xl ? '44' : '32';
+  const fontStyle = props.xl
+    ? 'font-semibold text-3xl'
+    : 'font-semibold text-xl';
+
+  return (
+    <span className={`inline-flex items-center text-gray-900 ${fontStyle}`}>
+      <img src="assets/images/logo.svg" alt="Logo" />
+      {AppConfig.site_name}
+    </span>
+  );
+};
+
+export { Logo };
